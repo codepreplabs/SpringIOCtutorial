152129890de8d2011dd26f58fc060ebdfbf468dc
thejavamonk <thejavamonk@gmail.com>
2015-06-01 18:19:24 +0530

    example for dependency injection of collections

---
+++ 07collectionsInjection/src/main/java/com/codepreplabs/app/TestSpring.java
@@ -0,0 +1,42 @@
+package com.codepreplabs.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+	static {
+
+		BasicConfigurator.configure();
+	}
+
+	@SuppressWarnings({ "unchecked", "unused" })
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		ArrayList<String> skills = (ArrayList<String>) applicationContext
+				.getBean("skills");
+
+		for (String skill : skills)
+			System.out.println(skill);
+
+		HashMap<String, String> addresses = (HashMap<String, String>) applicationContext
+				.getBean("addresses");
+		System.out.println(addresses.get("permanentAddress"));
+		System.out.println(addresses.get("temporaryAddress"));
+		
+		Properties properties = (Properties) applicationContext.getBean("credentials");
+		System.out.println(properties.getProperty("username"));
+		System.out.println(properties.getProperty("password"));
+
+	}
+
+}

1fb16e0bf2df68b33e86b2f54c4685ff1469eac1
thejavamonk <thejavamonk@gmail.com>
2015-06-02 12:55:53 +0530

    example for using the p-namespace

---
+++ 10pnamespace/src/main/java/com/codepreplabs/app/TestSpring.java
@@ -0,0 +1,34 @@
+package com.codepreplabs.app;
+
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.codepreplabs.bean.Person;
+
+public class TestSpring {
+
+	static {
+
+		BasicConfigurator.configure();
+	}
+
+	@SuppressWarnings({ "unchecked", "unused" })
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		
+		Person classic = (Person) applicationContext.getBean("jhonDoe-classic");
+		
+		System.out.println(classic.getName());
+		System.out.println(classic.getSpouse().getName());
+		
+		Person modern = (Person) applicationContext.getBean("jhonDoe-modern");
+		
+		System.out.println(modern.getName());
+		System.out.println(modern.getSpouse().getName());
+	}
+
+}
---
+++ 10pnamespace/src/main/java/com/codepreplabs/bean/Person.java
@@ -0,0 +1,24 @@
+package com.codepreplabs.bean;
+
+public class Person {
+
+	private String name;
+	private Person spouse;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Person getSpouse() {
+		return spouse;
+	}
+
+	public void setSpouse(Person spouse) {
+		this.spouse = spouse;
+	}
+
+}

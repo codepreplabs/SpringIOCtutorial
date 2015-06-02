6b0955eba91930ab15aa87788c5fb2c5ec8e9c77
thejavamonk <thejavamonk@gmail.com>
2015-06-02 14:01:40 +0530

    example on use of depends on attribute

---
+++ 12dependson/src/main/java/com/codepreplabs/bean/Employee.java
@@ -0,0 +1,33 @@
+package com.codepreplabs.bean;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	private Department department;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+}

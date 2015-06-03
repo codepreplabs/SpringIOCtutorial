afc0dc7f3dbfedf487ff5fc7923583b6699fea59
thejavamonk <thejavamonk@gmail.com>
2015-06-03 18:54:52 +0530

    example for use of @Resource annotation

---
+++ 19@Resource/src/main/java/com/codepreplabs/bean/Employee.java
@@ -0,0 +1,37 @@
+package com.codepreplabs.bean;
+
+import javax.annotation.Resource;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	
+	@Resource(name="department")
+	private Department department;
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+}

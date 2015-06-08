43e3bb482ac94c1bd77eb04f33fd89b77d5d4110
thejavamonk <thejavamonk@gmail.com>
2015-06-08 11:07:35 +0530

    example for using @Named and @Inject JSR 330 annotation.

---
+++ 23@InjectAnd@Named/src/main/java/com/codepreplabs/bean/LoginService.java
@@ -0,0 +1,35 @@
+package com.codepreplabs.bean;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Named
+public class LoginService {
+
+	int loginAttempts;
+
+	@Inject
+	LoginDAO loginDAO;
+	boolean isValid;
+
+	public String validateLogin(String username, String password) {
+		if (loginAttempts < 3) {
+			isValid = loginDAO.validateLogin(username, password);
+		} else {
+			System.out
+					.println("Account disabled due to incorrect credentials contact Administrator");
+		}
+
+		if (isValid) {
+
+			return "Successful login";
+		} else {
+
+			loginAttempts = loginAttempts++;
+			return "Failed to login";
+		}
+	}
+}
